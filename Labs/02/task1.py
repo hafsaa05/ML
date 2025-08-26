2eadbd20acd2b606b7fb8f67d7815e2996f83ed9
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-08-26 15:12:28 +0500

    Update task1.py

--- Labs/02/task1.py
+++ Labs/02/task1.py
@@ -1,1 +1,13 @@
-hhh
+import pandas as pd
+
+df1 = pd.read_csv('Lab2 D1A.csv')
+df2 = pd.read_csv('Lab2 D1B.csv')
+
+common_cols = list(set(df1.columns) & set(df2.columns))
+
+merged_df = pd.merge(df1, df2, on=common_cols, how='outer')
+
+merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()]
+
+print("Final Columns:", merged_df.columns)
+print("Final Shape:", merged_df.shape)
