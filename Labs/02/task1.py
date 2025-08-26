b7174a038343ea9eaba1e5649a6b6128fb566f1d
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-08-26 15:21:04 +0500

    Update task1.py

--- Labs/02/task1.py
+++ Labs/02/task1.py
@@ -1,14 +1,25 @@
 import pandas as pd
 
+# Part B
 df1 = pd.read_csv('Lab2 D1A.csv')
 df2 = pd.read_csv('Lab2 D1B.csv')
 
-common_cols = list(set(df1.columns) & set(df2.columns))
+common_cols_AB = list(set(df1.columns) & set(df2.columns))
 
-merged_df = pd.merge(df1, df2, on=common_cols, how='outer')
+merged_df = pd.merge(df1, df2, on=common_cols_AB, how='outer')
 
-#Drop Duplicate coloumns
+# Drop duplicate columns
 merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()]
 
-print("Final Columns:", merged_df.columns)
-print("Final Shape:", merged_df.shape)
+print("(D1A + D1B) Final Columns:", merged_df.columns)
+print("(D1A + D1B) Final Shape:", merged_df.shape)
+
+# Part C
+df3 = pd.read_csv('Lab2 D1C.csv')
+
+common_cols_AC = list(set(df1.columns) & set(df3.columns))
+
+comboAC = pd.merge(df1, df3, on=common_cols_AC, how='inner')
+
+print("(D1A + D1C) Final Columns:", comboAC.columns)
+print("(D1A + D1C) Final Shape:", comboAC.shape)
